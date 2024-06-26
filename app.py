3c37d367567beca3b8b2a9b70e49fd1e5e8f2758
AmirKhan <amirnasirkhan24@gmail.com>
2024-06-26 12:44:40 +0530

    Initial commit

--- app.py
+++ app.py
@@ -23,18 +23,23 @@ def upload_image():
     if file.filename == '':
         return redirect(request.url)
     if file:
-        image = Image.open(file).convert('L')
+        image = Image.open(file)
+        grayscale_image = image.convert('L')
         buffer = io.BytesIO()
-        image.save(buffer, format="PNG")
-        img_str = base64.b64encode(buffer.getvalue()).decode()
-        return render_template('preview.html', img_str=img_str)
+        grayscale_image.save(buffer, format="PNG")
+        buffer_value = buffer.getvalue()
+        img_str = base64.b64encode(buffer_value)
+        img_str_decoded = img_str.decode()
+        return render_template('preview.html', img_str=img_str_decoded)
 
 @app.route('/colorize', methods=['POST'])
 def colorize_image_route():
     image_data = request.form['image_data']
     image_bytes = base64.b64decode(image_data)
-    image = Image.open(io.BytesIO(image_bytes)).convert('L')
-    image_np = np.array(image)
+    image_buffer = io.BytesIO(image_bytes)
+    image = Image.open(image_buffer)
+    grayscale_image = image.convert('L')
+    image_np = np.array(grayscale_image)
     colorized_image_np = colorize_image(image_np)
     colorized_image = Image.fromarray(colorized_image_np)
     image_id = save_image_to_mongo(colorized_image)
@@ -42,23 +47,64 @@ def colorize_image_route():
 
 @app.route('/image/<image_id>')
 def show_image(image_id):
-    image_data = mongo.db.images.find_one({'_id': ObjectId(image_id)})
-    image = Image.open(io.BytesIO(image_data['data']))
-    buffer = io.BytesIO()
-    image.save(buffer, format="PNG")
-    img_str = base64.b64encode(buffer.getvalue()).decode()
-    return render_template('result.html', img_str=img_str)
+    object_id = ObjectId(image_id)
+    image_data = mongo.db.images.find_one({'_id': object_id})
+    image_bytes = image_data['data']
+    image_buffer = io.BytesIO(image_bytes)
+    image = Image.open(image_buffer)
+    output_buffer = io.BytesIO()
+    image.save(output_buffer, format="PNG")
+    output_value = output_buffer.getvalue()
+    img_str = base64.b64encode(output_value)
+    img_str_decoded = img_str.decode()
+    return render_template('result.html', img_str=img_str_decoded)
 
 def colorize_image(image_np):
-    colorized_image_np = cv2.applyColorMap(image_np, cv2.COLORMAP_JET)
+    image_8bit = cv2.normalize(image_np, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    colormap = cv2.COLORMAP_JET
+    colorized_image_np = cv2.applyColorMap(image_8bit, colormap)
     return colorized_image_np
 
 def save_image_to_mongo(image):
     buffer = io.BytesIO()
     image.save(buffer, format="PNG")
     img_data = buffer.getvalue()
-    image_id = mongo.db.images.insert_one({'data': img_data}).inserted_id
+    result = mongo.db.images.insert_one({'data': img_data})
+    image_id = result.inserted_id
     return image_id
+
+def validate_image(file):
+    if file and allowed_file(file.filename):
+        return True
+    return False
+
+def allowed_file(filename):
+    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+def get_image_format(filename):
+    return filename.rsplit('.', 1)[1].lower()
+
+def create_image_buffer(image, format):
+    buffer = io.BytesIO()
+    image.save(buffer, format=format.upper())
+    return buffer
+
+def encode_image(buffer):
+    buffer_value = buffer.getvalue()
+    encoded_image = base64.b64encode(buffer_value)
+    return encoded_image.decode()
+
+def decode_image_data(image_data):
+    image_bytes = base64.b64decode(image_data)
+    image_buffer = io.BytesIO(image_bytes)
+    return Image.open(image_buffer)
+
+def normalize_image(image_np):
+    return cv2.normalize(image_np, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+def apply_colormap(image_np, colormap=cv2.COLORMAP_JET):
+    return cv2.applyColorMap(image_np, colormap)
 
 if __name__ == '__main__':
     app.run(debug=True)
